da27c69b8ccdf816912d8910cdc6b99729a8f22a
Anderson Meira <anderson.meira@gmail.com>
2021-03-05 17:33:01 -0300

    feat(usuarios): create model manager

--- usuarios/models.py
+++ usuarios/models.py
@@ -4,6 +4,33 @@ from django.contrib.auth.models import (
     AbstractBaseUser,
     PermissionsMixin
 )
+class UsuarioManager(BaseUserManager):
+
+    def create_user(self, email, password=None):
+        usuario = self.model(email= self.normalize_email(email))
+
+        usuario.is_active = True
+        usuario.is_staff = False
+        usuario.is_superuser = False
+
+        if password:
+            usuario.set_password(password)
+
+        usuario.save()
+
+        return usuario
+
+    def create_superuser(self, email, password):
+        usuario = self.create_user(email=self.normalize_email(email), password=password)
+
+        usuario.is_active = True
+        usuario.is_staff = True
+        usuario.is_superuser = True
+
+        usuario.set_password(password)
+        usuario.save()
+
+        return usuario
 
 class Usuario(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(verbose_name="E-mail do usuário", max_length=194, unique=True)
@@ -12,6 +39,8 @@ class Usuario(AbstractBaseUser, PermissionsMixin):
     is_superuser = models.BooleanField(verbose_name="Usuário é um superusuario", default=False)
 
     USERNAME_FIELD = "email"
+
+    objects = UsuarioManager
 
     def __str__(self):
         return self.email
