0b583dc74ad8eed5c53ee142f0d88832a7385902
Anderson Meira <anderson.meira@gmail.com>
2021-03-05 12:25:00 -0300

    feat(usuarios): create user model

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    PermissionsMixin
+)
 
-# Create your models here.
+class Usuario(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(verbose_name="E-mail do usuário", max_length=194, unique=True)
+    is_active = models.BooleanField(verbose_name="Usuário está ativo", default=True)
+    is_staff = models.BooleanField(verbose_name="Usuário é da equipe de desenvolvimento", default=False)
+    is_superuser = models.BooleanField(verbose_name="Usuário é um superusuario", default=False)
+
+    USERNAME_FIELD = "email"
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = "Usuário"
+        verbose_name_plural = "Usuários"
+        db_table = "usuario"
